e35d79ac359012ba50bc1b6e17a7c057835a47f0
BT <71487741+qriousdev@users.noreply.github.com>
2022-02-18 00:41:25 -0800

    install all dev dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
 	}
+
+	// preprocess: [
+	// 	preprocess({
+	// 		scss: {
+	// 			prependData: `@use '${sassPath}global' as *;`
+	// 		}
+	// 	}),
+	// 	mdsvex({ extensions: ['.md', '.svx'] })
+	// ]
 };
 
 export default config;
