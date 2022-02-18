c4be5e64d164d468b44c3ac90291889b0e85011f
BT <71487741+qriousdev@users.noreply.github.com>
2022-02-18 00:49:51 -0800

    add the same config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,46 @@
-import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
+// import { routes } from './src/lib/data/routesData.js';
 
-/** @type {import('@sveltejs/kit').Config} */
+const filePath = dirname(fileURLToPath(import.meta.url));
+const sassPath = `${filePath}/src/lib/style/`;
+
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({}),
 
-	// preprocess: [
-	// 	preprocess({
-	// 		scss: {
-	// 			prependData: `@use '${sassPath}global' as *;`
-	// 		}
-	// 	}),
-	// 	mdsvex({ extensions: ['.md', '.svx'] })
-	// ]
+		// prerender: {
+		// 	entries: [...routes]
+		// },
+		vite: {
+			// plugins: [imagetools({ force: true })],
+
+			resolve: {
+				alias: {
+					$comp: path.resolve('./src/lib/components'),
+					$util: path.resolve('./src/lib/utils'),
+					$act: path.resolve('./src/lib/actions'),
+					$data: path.resolve('./src/lib/data'),
+					$stores: path.resolve('./src/lib/stores'),
+					$content: path.resolve('./src/lib/content')
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: `@use '${sassPath}global' as *;`
+			}
+		}),
+		mdsvex({ extensions: ['.md', '.svx'] })
+	],
+
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
